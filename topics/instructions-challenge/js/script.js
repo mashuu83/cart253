332d0f83f5c711d2954de9aaa0ab06f08447733e
Matthew Thompson <mashuu83@gmail.com>
2026-09-17 15:42:20 -0400

    Drew a Mountain
    Meet Mt.Drew, he has an accurate slanted snowcap just like real life

--- topics/instructions-challenge/js/script.js
+++ topics/instructions-challenge/js/script.js
@@ -21,5 +21,22 @@ function setup() {
 */
 function draw() {
     //Draw the Sky
-    background("#7cd3f2")
+    background("#7cd3f2");
+
+    //Draw a Mountain
+    push();
+    stroke("black")
+    strokeWeight(3);
+    fill("grey");
+    // Draw a triangle that is sitting at the bottom of the canvas and goes off the canvas on the left
+    triangle(-100, 720, 200, 360, 500, 720);
+    pop();
+
+    //Add a Snowcap
+    push();
+    noStroke();
+    fill("white");
+    //MATH!?! Heck no, we guess an check like MADMEN.
+    triangle(117, 460, 200, 360, 300, 480);
+    pop();
 }
