6b987f0541e3d17e8805c203556929e6bd2cca82
Matthew Thompson <46249660+mashuu83@users.noreply.github.com>
2026-09-23 19:36:26 -0400

    Added spinning triangle
    Had to figure out why triangle was being drawn in a weird spot, because I used WEBGL it is using a 3d coordinate system where 0,0 is the centre of the canvas instead of the top left.

--- topics/prototypes/instructions/instructions-prototype-1/js/script.js
+++ topics/prototypes/instructions/instructions-prototype-1/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Title of Project
- * Author Name
+ * Matthew Brandon Thompson
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -9,16 +9,24 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create a 720p Canvas for Illustration
+ * Uses WEBGL as the optional argument for renderer, as I am referencing the rotating triangle example from https://p5js.org/reference/p5/triangle/ and I assume WEBGL might be necessary for the animation aspects
 */
 function setup() {
-
+    createCanvas(1280, 720, WEBGL)
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Create the Illustration
+ * ADD MORE DETAILS WHEN I KNOW WHAT I ACTUALLY DREW:
 */
 function draw() {
+    //Set the background color
+    background("BlanchedAlmond");
+    //Draw a spinning triangle in the middle of the canvas
+    //Because WEBGL uses a 3D coordinate system, the centre of the screen is 0,0
+    rotateY(frameCount * 0.01);
+    triangle(0, -240, -200, 120, 200, 120);
 
 }
