abb53c608a412e461401deb232b2c8912ca652bb
Matthew Thompson <46249660+mashuu83@users.noreply.github.com>
2026-09-23 19:51:17 -0400

    Adjusted Triangle
    Adjusted size, added color and stroke, and experimented with adding additional shapes in and outside of the "push-pop" to try and better understand the rotation function.

--- topics/prototypes/instructions/instructions-prototype-1/js/script.js
+++ topics/prototypes/instructions/instructions-prototype-1/js/script.js
@@ -2,15 +2,15 @@
  * Title of Project
  * Matthew Brandon Thompson
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * The First Prototype Illustration for the 'Instructions' Assignment
+ * Initial idea is to make a field of rotating triangles
  */
 
 "use strict";
 
 /**
  * Create a 720p Canvas for Illustration
- * Uses WEBGL as the optional argument for renderer, as I am referencing the rotating triangle example from https://p5js.org/reference/p5/triangle/ and I assume WEBGL might be necessary for the animation aspects
+ * Uses WEBGL as the optional argument for renderer, as I am referencing the rotating triangle example from https://p5js.org/reference/p5/triangle/ and I assume WEBGL might be necessary for the animation
 */
 function setup() {
     createCanvas(1280, 720, WEBGL)
@@ -26,7 +26,16 @@ function draw() {
     background("BlanchedAlmond");
     //Draw a spinning triangle in the middle of the canvas
     //Because WEBGL uses a 3D coordinate system, the centre of the screen is 0,0
-    rotateY(frameCount * 0.01);
-    triangle(0, -240, -200, 120, 200, 120);
+    push();
+    rotateY(frameCount * 0.015);
+    stroke("BurlyWood");
+    strokeWeight(3);
+    fill("Cornsilk");
+    triangle(0, -340, -300, 220, 300, 220);
+    pop();
 
+    //Want to see if a new shape will also rotate
+    //ellipse(0, 0, 100);
+    //This is pretty cool, the Ellipse does not rotate (presumably becuase of the pop)
+    //Yep, when I comment out the pop, the ellipse "inherits" the colors and rotation... this can be cool to decorate the rotating triangle....
 }
