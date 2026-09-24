230b8e677bbd318d4fdece915f9fe62d31815a73
Matthew Thompson <mashuu83@gmail.com>
2026-09-24 15:29:00 -0400

    Update script.js
    Working with mr. furious, did step 2, change to red, step 3, change sky, step 4, add bird, we made the bird fly upwards

--- topics/weekly-challenges/variables-challenge/js/script.js
+++ topics/weekly-challenges/variables-challenge/js/script.js
@@ -23,6 +23,37 @@ let mrFurious = {
     }
 };
 
+// Our enemy the Sky
+let sky = {
+    fill: {
+        r: 160,
+        g: 180,
+        b: 200
+    }
+};
+
+// Our frenemie the BIRD
+let bird = {
+    position: {
+        x: -10,
+        y: 100
+    },
+
+    fill: {
+        r: 230,
+        g: 208,
+        b: 99
+    },
+
+    size: {
+        h: 10,
+        w: 20
+    },
+
+    velocity: 1,
+    speed: 1
+}
+
 /**
  * Create the canvas
  */
@@ -34,12 +65,22 @@ function setup() {
  * Draw (and update) Mr. Furious
  */
 function draw() {
-    background(160, 180, 200);
+    //Draw sky
+    background(sky.fill.r -= .15, sky.fill.g -= .15, sky.fill.b -= .15);
 
     // Draw Mr. Furious as a coloured circle
     push();
     noStroke();
-    fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+    // Make mr.furious turn red over time
+    fill(mrFurious.fill.r, mrFurious.fill.g -= 0.2, mrFurious.fill.b -= 0.2);
     ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+    pop();
+
+    // Adding bird left to right
+    push();
+
+    fill(bird.fill.r, bird.fill.g, bird.fill.b);
+    ellipse(bird.position.x += bird.speed, bird.position.y -= bird.speed * 0.25, bird.size.w, bird.size.h);
+
     pop();
 }
