f3c39c1ff36d9b6d4e5c04fc3ebc17d8d72077e9
Matthew Thompson <46249660+mashuu83@users.noreply.github.com>
2026-09-23 21:34:34 -0400

    Updated rotation speed and added some circles
    Messed with the variables for the roation speed to find something I really liked. Added some circles onto the triangle which rotate with it, but this is giving swiss cheese vibes, so I might explore making the circles rotate independently.

--- topics/prototypes/instructions/instructions-prototype-1/js/script.js
+++ topics/prototypes/instructions/instructions-prototype-1/js/script.js
@@ -1,9 +1,9 @@
 /**
- * Title of Project
+ * Funny Color Names and Slow Rotations
  * Matthew Brandon Thompson
  * 
  * The First Prototype Illustration for the 'Instructions' Assignment
- * Initial idea is to make a field of rotating triangles
+ * Trying to choose colors for this project from the CSS color list that I think have the funniest/cutest names. Want to explore the concept of shapes rotating slowly. But not like, too slowly.
  */
 
 "use strict";
@@ -19,19 +19,25 @@ function setup() {
 
 /**
  * Create the Illustration
- * ADD MORE DETAILS WHEN I KNOW WHAT I ACTUALLY DREW:
+ * Want to start with a slowly rotating triangle and build from there
 */
 function draw() {
     //Set the background color
     background("BlanchedAlmond");
     //Draw a spinning triangle in the middle of the canvas
-    //Because WEBGL uses a 3D coordinate system, the centre of the screen is 0,0
     push();
-    rotateY(frameCount * 0.015);
+    rotateY(frameCount * 0.004);
     stroke("BurlyWood");
     strokeWeight(3);
     fill("Cornsilk");
+    //Because WEBGL uses a 3D coordinate system, the centre of the screen is 0,0
     triangle(0, -340, -300, 220, 300, 220);
+    //Add some more shapes inside the triangle that should rotate with it
+    stroke("LightCoral");
+    fill("LemonChiffon");
+    ellipse(-150, 150, 100);
+    ellipse(0, -150, 100, 150);
+    ellipse(-80, 150, 100, 75);
     pop();
 
     //Want to see if a new shape will also rotate
